20dd703a9e9701ef3f4f10f9987c210a5a08811d
aauvray <auvrayarman@gmail.com>
2024-08-26 12:49:27 +0200

    hh

--- smart-road/src/lib.rs
+++ smart-road/src/lib.rs
@@ -1,3 +1,4 @@
 //lib.rs permet juste d'utiliser l'analyser rust sans rentrer directement dans le dossier 
 pub mod matrice;
+pub mod interface;
 
--- smart-road/src/main.rs
+++ smart-road/src/main.rs
@@ -1,6 +1,11 @@
 use smart_road::matrice::matrice;
+use smart_road::interface::interface;
+
+const LENGTH: u8 = 30;
+const WIDTH: u8 = 20;
 
 fn main() {
-    let result = matrice(3, 4);
+    let result = matrice(LENGTH, WIDTH);
     println!("{:?}", result);
+    interface(LENGTH, WIDTH);
 }

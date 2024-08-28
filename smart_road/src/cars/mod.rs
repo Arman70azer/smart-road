edb5781bb7c86c9a30cd3a14e68d47ed254137b4
Arman <auvrayarman@gmail.com>
2024-08-28 14:41:34 +0200

    test fusion with car dir

---
+++ smart_road/src/cars/mod.rs
@@ -0,0 +1,50 @@
+use crate::matrix::sub_mod_texture::Texture;
+use sdl2::video::WindowContext;
+use sdl2::render::TextureCreator;
+
+#[derive(PartialEq)]
+pub enum Destinations {
+    North,
+    South,
+    East,
+    West,
+}
+
+pub struct Car<'a> {
+    pub row: u32,
+    pub column: u32,
+    pub texture: Texture<'a>,
+    pub path: Vec<(u32, u32)>,
+    pub position: (u32, u32),
+    pub speed: u8,
+}
+
+impl<'a> Car<'a> {
+    pub fn new(spawn:Destinations, destination: Destinations, texture_creator: &'a TextureCreator<WindowContext>)->Self{
+        let positions = match spawn {
+            Destinations::North => north_spawn(destination),
+            Destinations::South => south_spawn(destination)
+        }
+        Car()
+    }
+}
+
+fn north_spawn(destination: Destinations)->(u32, u32){
+    if destination == Destinations::West {
+        return (0, 9);
+    }
+    if destination== Destinations::South{
+        return (0, 10);
+    }
+    (0, 11)
+}
+
+fn south_spawn(destination: Destinations)->(u32, u32){
+    if destination == Destinations::West {
+        return (0, 9);
+    }
+    if destination== Destinations::South{
+        return (0, 10);
+    }
+    (0, 11)
+}

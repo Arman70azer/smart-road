6869a4a63e13009562a0a3ce7062c6a2a6d445f3
njeanfre <nangy.pro@gmail.com>
2024-08-30 09:46:33 +0200

    direction ok

--- smart_road/src/main.rs
+++ smart_road/src/main.rs
@@ -4,44 +4,32 @@ use smart_road::matrix::{matrix_and_canva, ROW, COLUMN}; // Import the draw_matr
 use smart_road::cars::{Car, Destinations};
 use sdl2::image::InitFlag;
 use sdl2::pixels::Color;
-
 const WIDTH: i32 = 800; // Example width
 const HEIGHT: i32 = 800; // Example height
 const SQUARE_SPEED: i32= 1;
-
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-
-
     sdl2::image::init(InitFlag::PNG | InitFlag::JPG | InitFlag::WEBP).unwrap();
-
     let window = video_subsystem
         .window("SDL2 Window", WIDTH as u32, HEIGHT as u32)
         .position_centered()
         .build()
         .unwrap();
-
     let mut canvas = window.into_canvas().build().unwrap();
-
-
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     let texture_creator = canvas.texture_creator();
-
     matrix_and_canva(&mut canvas ,HEIGHT, WIDTH );
     
     canvas.present();
-
     let cell_size_width = WIDTH / COLUMN;
     let cell_size_height = HEIGHT / ROW;
     let cell_size = cell_size_width.min(cell_size_height);
-
     // Main loop placeholder
     let mut event_pump = sdl_context.event_pump().unwrap();
      // Créer un vecteur pour stocker les voitures
      let mut cars: Vec<Car> = Vec::new();
-
      'running: loop {
          // Gestion des événements
          for event in event_pump.poll_iter() {
@@ -73,7 +61,6 @@ fn main() {
          for car in &mut cars{
              car.update_position();
          }
-
          cars.retain(|car| {
             car.column >= 0
                 && car.column <= WIDTH
@@ -84,9 +71,7 @@ fn main() {
          // Effacer le canevas
          canvas.set_draw_color(Color::RGB(0, 0, 0));
          canvas.clear();
-
          matrix_and_canva(&mut canvas ,HEIGHT, WIDTH );
-
  
          // Dessiner chaque voitures
          canvas.set_draw_color(Color::RGB(255, 0, 0));

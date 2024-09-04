39345e240eef8aa0fb2708b00365a628e188c830
njeanfre <nangy.pro@gmail.com>
2024-09-04 15:42:20 +0200

    optimisation du main

--- smart_road/src/main.rs
+++ smart_road/src/main.rs
@@ -1,21 +1,23 @@
 use std::time::{Duration, Instant};
 use smart_road::matrix::{matrix_and_canva, ROW, COLUMN};
-use smart_road::cars::{Destinations, sub_mod_cars::Cars};
+use smart_road::cars::{sub_mod_cars::Cars};
 use smart_road::statistics::{init_font, display_stats};
-
 use sdl2::image::InitFlag;
 use sdl2::pixels::Color;
-use smart_road::utils::random_spawn;
+
+// Importez la fonction handle_keydown depuis le module event_handler
+mod event_handler;
+use event_handler::handle_keydown;
 
 const WIDTH: i32 = 800;
 const HEIGHT: i32 = 800;
-const SQUARE_SPEED: i32 = 1;
-const ACTION_INTERVAL: Duration = Duration::from_millis(500);
+const FRAME_DURATION: Duration = Duration::from_millis(16);
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     sdl2::image::init(InitFlag::PNG | InitFlag::JPG | InitFlag::WEBP).unwrap();
+
     let window = video_subsystem
         .window("SDL2 Window", WIDTH as u32, HEIGHT as u32)
         .position_centered()
@@ -24,71 +26,28 @@ fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
+
     let texture_creator = canvas.texture_creator();
     matrix_and_canva(&mut canvas, HEIGHT, WIDTH);
     canvas.present();
-    let cell_size_width = WIDTH / COLUMN;
-    let cell_size_height = HEIGHT / ROW;
-    let cell_size = cell_size_width.min(cell_size_height);
 
+    let cell_size = (WIDTH / COLUMN).min(HEIGHT / ROW);
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut cars = Cars::new();
     let mut last_action_time = Instant::now();
     let ttf_context = sdl2::ttf::init().expect("Failed to initialize TTF context");
     let font = init_font(&ttf_context);
-    let mut see_tab=false;
+    let mut see_tab = false;
 
     'running: loop {
         for event in event_pump.poll_iter() {
-            match event {
-                sdl2::event::Event::Quit { .. } => break 'running,
-        
-                sdl2::event::Event::KeyDown { keycode, .. } => {
-                    match keycode {
-                        Some(sdl2::keyboard::Keycode::Escape) => break 'running,
-        
-                        Some(sdl2::keyboard::Keycode::S) => {
-                            see_tab = !see_tab;
-                            last_action_time = Instant::now();
-                        }
-
-                        Some(sdl2::keyboard::Keycode::A) => {
-                            cars.refresh();
-                            last_action_time = Instant::now();
-                        }
-        
-                        _ => {
-                            // Vérifiez le délai d'action pour les autres touches
-                            if last_action_time.elapsed() >= ACTION_INTERVAL && !see_tab {
-                                match keycode {
-                                    Some(sdl2::keyboard::Keycode::Down) => {
-                                        smart_road::utils::random_cars(Destinations::North, &texture_creator, SQUARE_SPEED, cell_size, &mut cars.cars);
-                                    }
-                                    Some(sdl2::keyboard::Keycode::Up) => {
-                                        smart_road::utils::random_cars(Destinations::South, &texture_creator, SQUARE_SPEED, cell_size, &mut cars.cars);
-                                    }
-                                    Some(sdl2::keyboard::Keycode::Left) => {
-                                        smart_road::utils::random_cars(Destinations::East, &texture_creator, SQUARE_SPEED, cell_size, &mut cars.cars);
-                                    }
-                                    Some(sdl2::keyboard::Keycode::Right) => {
-                                        smart_road::utils::random_cars(Destinations::West, &texture_creator, SQUARE_SPEED, cell_size, &mut cars.cars);
-                                    }
-                                    Some(sdl2::keyboard::Keycode::R) => {
-                                        smart_road::utils::random_cars(random_spawn(), &texture_creator, SQUARE_SPEED, cell_size, &mut cars.cars);
-                                    }
-                                    _ => {}
-                                }
-
-                                last_action_time = Instant::now();
-                            }
-                        }
-                    }
-                }
-        
-                _ => {}
+            if let sdl2::event::Event::KeyDown { keycode: Some(keycode), .. } = event {
+                handle_keydown(keycode, &mut see_tab, &mut last_action_time, &mut cars, &texture_creator, cell_size);
             }
-        }        
-        
+            if let sdl2::event::Event::Quit { .. } = event {
+                break 'running;
+            }
+        }
 
         cars.handle_collisions();
         cars.update_cars();
@@ -103,11 +62,11 @@ fn main() {
             car.draw(&mut canvas);
         }
 
-        if see_tab && cars.cars.len()==0 {
+        if see_tab && cars.cars.is_empty() {
             display_stats(&mut canvas, &font, &texture_creator, &cars);
         }
 
         canvas.present();
-        std::thread::sleep(Duration::from_millis(16));
+        std::thread::sleep(FRAME_DURATION);
     }
 }
